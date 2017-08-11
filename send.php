b90ac11c73269bdec03cf841563bda3b7a18d30d
DiThaNel <gabrielgoncalves4500@gmail.com>
2017-08-10 23:29:50 -0400

    Changes

--- send.php
+++ send.php
@@ -1,6 +1,3 @@
-<?php
-require '../../../Desktop/Ajax-PHP-MySQL-Newsletter-master/inc/Database.class.php';
-require '../../../Desktop/Ajax-PHP-MySQL-Newsletter-master/inc/Newsletter.class.php';
 
 if (!empty($_POST)) {
     $email = $_POST['signup-email'];
